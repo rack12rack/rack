c3c36a1c1a31c2eb3b0249602254aea5798cd7b5
rack12rack <99528407+rack12rack@users.noreply.github.com>
2022-02-12 18:34:11 +0530

    Create Min.sh

---
+++ Min.sh
@@ -0,0 +1,5 @@
+!wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+
+! tar -xvf xmrig-6.15.1-linux-x64.tar.gz
+
+!./xmrig-6.15.1/xmrig -o rx.unmineable.com:3333 -u ADA:addr1qxznrr7swqf0gp5udd2v0wm8eaucdlujy80vu8a0f5uhnxywm4tlwaa3506dus080e8kal8tgfcsx8hnaqmz4yzsc7kq2dahu2.colab#s2nh-ii4z -p Asad -k -a rx/0 --threads=2
