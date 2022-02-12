6b3bd3212130966505e302fb655a4ae383fe5140
rack12rack <99528407+rack12rack@users.noreply.github.com>
2022-02-12 18:39:59 +0530

    Update Min.sh

--- Min.sh
+++ Min.sh
@@ -1,5 +1,10 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
 
-tar -xvf xmrig-6.15.1-linux-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 
-./xmrig-6.15.1/xmrig -o rx.unmineable.com:3333 -u ADA:addr1qxznrr7swqf0gp5udd2v0wm8eaucdlujy80vu8a0f5uhnxywm4tlwaa3506dus080e8kal8tgfcsx8hnaqmz4yzsc7kq2dahu2.colab#s2nh-ii4z -p Asad -k -a rx/0 --threads=2
+ls
+
+cd xmrig-6.15.0
+
+./xmrig -o sg.minexmr.com:443 -u 466qu8xzgtDXCr52mZxubW2yCFmdcAuLpHKECRmaCRn26V6LgNi5ttP74dQ7g2mUJNRzTqgDM6PUZQhaG45DgUx43kRxAoY -k --tls --rig-id rig
+
